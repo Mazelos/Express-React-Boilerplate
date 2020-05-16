65559748efa7f11995319afdccd0c4117d7dacca
Madzelos <madze706@gmail.com>
2020-05-16 19:43:03 +0200

    added --mode flag in package.json scripts, changed order of the webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,21 @@
 const path = require("path");
 
 module.exports = {
-  mode: 'development',
   entry: path.resolve(__dirname, 'src', 'client'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    // publicPath: '/',
   },
   module: {
     rules: [
       {
-      test: /\.jsx?/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']   //this will first tranform css in js code and then inject into the DOM
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+      test: /\.jsx?/,   //this will look for any file ending with 'js' or 'jsx'
+      loader: 'babel-loader',
+      exclude: /node_modules/,  // i'm not really sure on this ...
       }
     ]
   }
